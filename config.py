f77743bcb1c8d6c9dc52a63dbee055bb43e8ce40
STARS2312 <deordeor31@gmail.com>
2026-02-22 13:46:07 +0300

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
 
-TOKEN = os.getenv("8060964587:AAFp9CaRdt1VkIEbfOAe24dxyUY4ZQS2fBo")
-ADMIN_ID = int(os.getenv("1112682127"))
+TOKEN = os.getenv(8060964587:AAFp9CaRdt1VkIEbfOAe24dxyUY4ZQS2fBo)
+
+admin_id = os.getenv(1112682127)
+ADMIN_ID = int(admin_id) if admin_id else None
